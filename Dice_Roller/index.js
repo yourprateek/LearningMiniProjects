a3823d250d265aa53ae41a5888d6ce8265caf901
yourprateek <prateekg.dev@gmail.com>
2026-08-16 17:14:28 +0530

    Dice Roller Completed

--- Dice_Roller/index.js
+++ Dice_Roller/index.js
@@ -1,1 +1,18 @@
 console.log("JS Linked");
+
+function rollDice(){
+    const numOfDice = document.getElementById("numOfDice").value;
+    const diceResult = document.getElementById("diceResult");
+    const diceImages = document.getElementById("diceImages");
+    const values = [];
+    const images = [];
+
+    for(let i=0; i< numOfDice; i++){
+        const value = Math.floor(Math.random()*6) + 1;
+        values.push(value);
+        images.push(`<img src="SVGs/dice-${value}-svgrepo-com.svg" alt="Dice's Number: ${value}">`);
+    }
+    diceResult.textContent = `Dice: ${values.join(', ')}`;
+    diceImages.innerHTML = images.join('');
+
+}
