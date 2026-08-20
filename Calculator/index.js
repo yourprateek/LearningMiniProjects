cf68ccd46c9247ce8ebf63ddc83aa56730a1d907
yourprateek <prateekg.dev@gmail.com>
2026-08-21 01:15:08 +0530

    Some more js practice and calculator color edited

--- Calculator/index.js
+++ Calculator/index.js
@@ -3,3 +3,15 @@ console.log("JS Linked");
 // let username= window.prompt("Enter your UserName: ").trim();
 // username = username.charAt(0).toUpperCase() + username.slice(1, username.indexOf(" ")+1).toLowerCase() + username.charAt(username.indexOf(" ")+1).toUpperCase() + username.slice(username.indexOf(" ")+2).toLowerCase();
 // console.log(username);
+
+// let numbers = [1, 2, 3, 5, 6, 7, 9, 10];
+// function display(element){
+//     console.log(element);
+// }
+// function double(element, index, array){
+//     array[index] = element * 2;
+// }
+
+// console.log("The members of array are: ");
+// numbers.forEach(double);
+// numbers.forEach(display);
