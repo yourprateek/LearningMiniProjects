59c30212967769558f31d595a237a38c765897b7
yourprateek <prateekg.dev@gmail.com>
2026-08-23 00:49:06 +0530

    just to save this

--- Calculator/index.js
+++ Calculator/index.js
@@ -11,7 +11,49 @@ console.log("JS Linked");
 // function double(element, index, array){
 //     array[index] = element * 2;
 // }
-
 // console.log("The members of array are: ");
 // numbers.forEach(double);
 // numbers.forEach(display);
+
+class Account{
+    #balance;
+    constructor(newfullName, newbalance, newfavColor){
+        this.fullName = newfullName;
+        this.balance = newbalance;
+        this.favColor = newfavColor;
+
+        [this.firstName = '', this.lastName = ''] = this.fullName.split(" ");
+    }
+
+    set balance(nicebalance){
+        if ( typeof nicebalance === "number") {
+            this.#balance = nicebalance;
+        }else console.error("Balance must be a number above -1");
+    }
+
+    get balance(){
+        return this.#balance;
+    }
+
+    
+};
+
+const myAccount = new Account("Prateek Gupta", 100, 'black');
+
+console.log(myAccount.balance);
+myAccount.balance = 1000;
+console.log(myAccount.balance);
+console.log(`This Account is of ${myAccount.firstName} ${myAccount.lastName}`);
+
+const aryanAccount = new Account("Aryan Sahu", 0, 'white');
+
+console.log(aryanAccount.balance);
+aryanAccount.balance = -100;
+console.log(aryanAccount.balance);
+console.log(`This Account is of ${aryanAccount.firstName} ${aryanAccount.lastName}`);
+
+const user = { username: 'Prateek', age: 21 };
+
+// Extract 'username' and rename the local variable to 'name'
+const { username: myname } = user;
+console.log(`${myname} is my name`); // 'Prateek'
